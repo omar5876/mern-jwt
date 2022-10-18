8c59d40f7c1b95c610ca0f012159af764a51e513
unknown <omarramirez5876@gmail.com>
2022-10-17 23:01:03 -0500

    adding react router dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,25 @@
-import Header from "./components/Header"
-import './app.css'
+import Header from "./components/Header";
+import {Routes, Route} from 'react-router-dom';
+import Login from "./components/Login";
+import Singup from "./components/Singup";
+import Welcome from "./components/Welcome";
+import './app.css';
 
 function App() {
 
 
   return (
-    <div>
+    <>
+    
       <Header/>
-    </div>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Singup/>}/>
+        <Route path="/user" element={<Welcome/>}/>
+      </Routes>
+    
+    
+    </>
   )
 }
 
