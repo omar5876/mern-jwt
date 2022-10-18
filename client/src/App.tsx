a49d98d4e1cb776f4943b6dc4a5efd9e2caa050e
unknown <omarramirez5876@gmail.com>
2022-10-17 21:47:27 -0500

    stating with the frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+import './app.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Header() {
+  return (
+    <div className='bg-blue-400 text-white flex justify-between p-2'>
+            <h3 className='text-3xl'>MernAuth</h3>
+            <div className='flex w-2/12 justify-evenly'>
+                <h4 className='rounded-xl p-2 hover:bg-blue-300'>Login</h4>
+                <h4 className='rounded-xl p-2 hover:bg-blue-300 '>Signup</h4>
+            </div>
+    </div>
+  )
+}
+
+export default Header
